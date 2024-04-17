620cb1e93fea67fa35503f4b1ed67f21b5973d46
LexPearson <139921639+Wa5Fa@users.noreply.github.com>
2024-04-17 18:40:05 +0300

    🍃 init: start the project

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  useTabs: false,
+  printWidth: 120,
+  semi: true,
+  plugins: []
+};
